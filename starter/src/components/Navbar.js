ac8d250e8253f4698859b59988b4502461a468c9
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-12-18 21:23:26 +0530

    minor changes

--- starter/src/components/Navbar.js
+++ starter/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { CartIcon, YesIcon, NoIcon } from "../icons";
+import { CartIcon } from "../icons";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
