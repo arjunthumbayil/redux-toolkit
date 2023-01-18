7bc1156a6d34ea0a6eafef92ba94ea832296a783
Arjun Thumbayil <arjunthumbayil@gmail.com>
2023-01-18 11:11:56 +0530

    modal complete

--- starter/src/App.js
+++ starter/src/App.js
@@ -7,6 +7,8 @@ import { useEffect } from "react";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,7 +16,8 @@ function App() {
   }, [cartItems]);
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
+
       <Navbar />
       <CartContainer />
     </main>
